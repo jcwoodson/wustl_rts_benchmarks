d3c0a588ad7c0174fb09a689dcbd86d3704f6a9a
jcwoodson <josephcwoodson@wustl.edu>
2014-08-04 15:44:54 -0500

    Added experiment data

---
+++ experiments/REU 2014 Results/REU2014FileReader/REU2014FileReader/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace REU2014FileReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string csv = "";
+            string csv2 = "";
+            for (int i = 25000; i <= 500000; i += 25000)
+            {
+                for (int j = 1; j <= 16; j++)
+                {
+                    csv += "cilkp," + i + "," + j + "," + File.ReadAllText("timing-sort-cilkp-" + i + "-" + j).Split('|')[1].Replace(" ", "") + "\n";
+                    string[] lines = File.ReadAllLines("timing-sort-cilkp-" + i + "-" + j);
+                    double max = 1337;
+                    foreach (string l in lines)
+                    {
+                        string num = l.Split(' ')[0];
+                        if (num.Length < 5)
+                            break;
+                        double d = Double.Parse(num);
+                        if (d/max > 1.20)
+                            break;
+                        max = d;
+                    }
+                    csv2 += "cilkp," + i + "," + j + "," + (max + 0.000001) + "\n";
+                }
+            }
+            for (int i = 25000; i <= 500000; i += 25000)
+            {
+                for (int j = 1; j <= 16; j++)
+                {
+                    csv += "omp," + i + "," + j + "," + File.ReadAllText("timing-sort-omp-" + i + "-" + j).Split('|')[1].Replace(" ", "") + "\n";
+                    string[] lines = File.ReadAllLines("timing-sort-omp-" + i + "-" + j);
+                    double max = 1337;
+                    foreach (string l in lines)
+                    {
+                        string num = l.Split(' ')[0];
+                        if (num.Length < 5)
+                            break;
+                        double d = Double.Parse(num);
+                        if (d/max > 1.20)
+                            break;
+                        max = d;
+                    }
+                    csv2 += "omp," + i + "," + j + "," + (max + 0.000001) + "\n";
+                }
+            }
+            File.WriteAllText("000-timing-sort.csv", csv);
+            File.WriteAllText("000-timing-sort-no-outliers.csv", csv2);
+        }
+    }
+}
---
+++ experiments/REU 2014 Results/REU2014FileReader/REU2014FileReader/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("REU2014FileReader")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("REU2014FileReader")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("23580aa4-7a99-4095-a6ed-a1350ad398f1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
